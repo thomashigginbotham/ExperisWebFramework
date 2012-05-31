ad79a16f473fed375781c8a5a4dcda0598b5a3e6
Thomas Higginbotham <thomas@thomashigginbotham.com>
2012-05-31 16:23:12 -0400

    Included framework scripts

---
+++ js/global.js
@@ -0,0 +1,17 @@
+﻿(function () {
+	// Run startup scripts
+	$xu.includeScript($x.path + 'experis.polyfills.js', function () {
+		$xp.placeholder(); // Add @placeholder support
+		$xp.video();       // Add support for HTML 5 video
+	});
+
+	$xu.includeScript($x.path + 'experis.widgets.js', function () {
+		// Add code for any Experis widgets you need to use
+	});
+
+	$xf.fixColumnPercentageWidths(); // Fix browser rendering of subpixel values in column widths
+
+	/* --
+	   Add your own scripts here
+	   -- */
+})();
