a5eb3f50d5c928b43bd45ae73904c1d91f113970
Thomas Higginbotham <thomas@thomashigginbotham.com>
2012-06-12 13:25:25 -0400

    Revised cubic bezier functions for accuracy

--- js/experis.fx.js
+++ js/experis.fx.js
@@ -10,35 +10,63 @@ if (!experis) { alert('You must include experis.utils.js before using Experis FX
 experis.fx = {
 	trans: {
 		linear: [0, 0, 1, 1],
-		easeIn: [0, 0, .58, 1],
-		easeOut: [1, 0, 1, 1],
-		swing: [-1, 0, -1, 1],
-		bounceIn: [-1, 0, 1, 1]
+		easeIn: [.42, 0, 1, 1],
+		easeOut: [0, 0, .58, 1],
+		easeInOut: [.42, 0, .58, 1],
+		bounceIn: [.42, -.42, 1, 1],
+		bounceOut: [.17, .67, .58, 1.3]
 	},
-	// Note: this function needs work. The math is currently wrong, but it does allow for basic easing.
 	getBezierTransform: function (start, end, steps, bezier) {
 		var transValues = [];
 
 		for (var n = 1; n < steps; n++) {
 			var t = n / steps;
-
-			var a = 1 - 3 * bezier[2] + 3 * bezier[0];
-			var b = 3 * bezier[2] - 6 * bezier[0];
-			var c = 3 * bezier[0];
-			var e = 1 - 3 * bezier[3] + 3 * bezier[1];
-			var f = 3 * bezier[3] - 6 * bezier[1];
-			var g = 3 * bezier[1];
-
-			var x = (((a * t) + b) * t + c) * t;
-			var y = (((e * x) + f) * x + g) * x;
+			var y = $xfx.spline(t, bezier[0], bezier[1], bezier[2], bezier[3]);
 
 			transValues.push(start + y * (end - start));
 		}
 
 		transValues.unshift(start);
 		transValues.push(end);
-		console.log(transValues);
+
 		return transValues;
+	},
+	/*
+	* spline (number, number, number, number, number)
+	* x: The x position on a cartesian plane in the range 0 - 1
+	* x1, y1, x2, y2: Coordinate points representing the handles of a cubic bezier
+	*/
+	spline: function (x, x1, y1, x2, y2) {
+		var a = function (a1, a2) { return 1.0 - 3.0 * a2 + 3.0 * a1; };
+		var b = function (a1, a2) { return 3.0 * a2 - 6.0 * a1; };
+		var c = function (a1) { return 3.0 * a1; };
+
+		var calcBezier = function (t, a1, a2) {
+			return ((a(a1, a2) * t + b(a1, a2)) * t + c(a1)) * t;
+		};
+
+		var getSlope = function (t, a1, a2) {
+			return 3.0 * a(a1, a2) * t * t + 2.0 * b(a1, a2) * t + c(a1);
+		};
+
+		var getTForX = function (x) {
+			var t = x;
+
+			for (var n = 0; n < 4; ++n) {
+				var slope = getSlope(t, x1, x2);
+
+				if (slope === 0.0) return t;
+
+				var curX = calcBezier(t, x1, x2) - x;
+				t -= curX / slope;
+			}
+
+			return t;
+		};
+
+		if (x1 === y1 && x2 === y2) return x; // Linear
+
+		return calcBezier(getTForX(x), y1, y2);
 	}
 };
 
