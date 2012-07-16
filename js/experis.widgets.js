07eb2c529f7ce889e077c7061cfe800f64a9b813
Thomas Higginbotham <thomas@thomashigginbotham.com>
2012-07-16 12:07:50 -0400

    Bug fix for content slider
    If navigation is omitted, error occured trying to set className property
    on navigation items

--- js/experis.widgets.js
+++ js/experis.widgets.js
@@ -315,14 +315,16 @@ experis.widgets = {
 						runTransition(slideNum, function () {
 							curSlide = slideNum;
 
-							// Add "selected" class to current navigation item
-							var items = NW.Dom.select('.experis-slider > ul li', wrapper.parentNode);
+							if (nav.length === 1) {
+								// Add "selected" class to current navigation item
+								var items = NW.Dom.select('.experis-slider > ul li', wrapper.parentNode);
 
-							for (var n = 0, item; item = items[n++];) {
-								item.className = item.className.replace(' selected', '');
+								for (var n = 0, item; item = items[n++];) {
+									item.className = item.className.replace(' selected', '');
+								}
+
+								items[curSlide - 1].className += ' selected';
 							}
-
-							items[curSlide - 1].className += ' selected';
 
 							if (mouseStatus === 'out') startSlider();
 						});
